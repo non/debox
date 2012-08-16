f4d1aea31dd06b304ce07fc5a68678f10fece76a
Erik Osheim <d_m@plastic-idolatry.com>
2012-08-16 12:27:30 -0400

    initial benchmark for Util.array macro

---
+++ benchmark/src/main/scala/debox/benchmark/LiteralMacroBenchmark.scala
@@ -0,0 +1,37 @@
+package debox.benchmark
+
+import com.google.caliper.Param
+
+import debox.Util.array
+
+object LiteralMacroBenchmarks extends MyRunner(classOf[LiteralMacroBenchmarks])
+class LiteralMacroBenchmarks extends MyBenchmark {
+
+  def timeApply0(reps:Int) = run(reps)(Array[Int]().length)
+  def timeMacro0(reps:Int) = run(reps)(array[Int]().length)
+
+  def timeApply1(reps:Int) = run(reps)(Array(1).length)
+  def timeMacro1(reps:Int) = run(reps)(array(1).length)
+
+  def timeApply2(reps:Int) = run(reps)(Array(1,2).length)
+  def timeMacro2(reps:Int) = run(reps)(array(1,2).length)
+
+  def timeApply4(reps:Int) = run(reps)(Array(1,2,3,4).length)
+  def timeMacro4(reps:Int) = run(reps)(array(1,2,3,4).length)
+
+  def timeApply8(reps:Int) = run(reps)(Array(1,2,3,4,5,6,7,8).length)
+  def timeMacro8(reps:Int) = run(reps)(array(1,2,3,4,5,6,7,8).length)
+
+  def timeApply16(reps:Int) = run(reps)(Array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16).length)
+  def timeMacro16(reps:Int) = run(reps)(array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16).length)
+
+  def timeApply32(reps:Int) = run(reps)(Array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32).length)
+  def timeMacro32(reps:Int) = run(reps)(array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32).length)
+
+  def timeApply64(reps:Int) = run(reps)(Array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64).length)
+  def timeMacro64(reps:Int) = run(reps)(array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64).length)
+
+  def timeApply128(reps:Int) = run(reps)(Array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128).length)
+  def timeMacro128(reps:Int) = run(reps)(array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128).length)
+  
+}
